8b7216758e700c23c03f841d7580c4d61a1e8cf7
BettinaShirley <bettina.shirley@gmail.com>
2025-08-24 19:26:10 +0100

    moving files for folder consistency

--- messagerLibrary.py
+++
@@ -1,5 +0,0 @@
-import pywhatkit 
-pywhatkit.sendwhatmsg_instantly('+919940160617', 'Hi mom', wait_time=5)
-pywhatkit.sendwhats_image('+919940160617', 'sampleImage.jpeg', 'Hello from Python!', wait_time=10)
-
- 
