22eb9ad42ed25c49cb56b244a6a9f20a874f1a6d
DerrickUnleashed <derricksamuel.r@gmail.com>
2025-09-05 01:10:56 +0530

    Fix: Installing all dependencies each run

--- backend/clearCache.sh
+++ backend/clearCache.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# cd backend
-# rm -rf .wwebjs*
+cd backend
+rm -rf node*
+rm -rf package-lock.json
+npm install
+rm -rf .wwebjs*
 lsof -ti :3001 | xargs kill -9 2>/dev/null
 lsof -ti :5173 | xargs kill -9 2>/dev/null
